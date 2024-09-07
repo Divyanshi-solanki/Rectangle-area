2c22d7690a8bb1c6c904851b56d03568ab4d80e2
Divyanshi Solanki <161625343+Divyanshi-solanki@users.noreply.github.com>
2024-09-07 16:51:46 +0530

    Add files via upload

---
+++ Area of rectengle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float l,b;
+   cout<<"Enter the lenght : ";
+   cin>>l;
+   cout<<"Enter the breadth : ";
+   cin>>b;
+   
+   cout<<"Area of rectangle is : "<<l*b;
+
+    return 0;
+}
